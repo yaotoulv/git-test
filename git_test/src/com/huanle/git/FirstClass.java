014d7f3eb66528e2b97a53b063353bbefef8fe54
y1 <yaotoulv@qq.com>
2020-05-03 16:54:59 +0800

    使用idea第一次提交版本

---
+++ git_test/src/com/huanle/git/FirstClass.java
@@ -0,0 +1,19 @@
+package com.huanle.git;
+
+public class FirstClass {
+    public static void main(String[] args) {
+        System.out.println("Hello Git!");
+        System.out.println("Hello Git!");
+        System.out.println("Hello Git!");
+        System.out.println("Hello Git!");
+        System.out.println("Hello Git!");
+        System.out.println("Hello Git!");
+        System.out.println("Hello Git!");
+        System.out.println("Hello Git!");
+    }
+    public void test(){
+        int i = 0;
+        i += 10;
+    }
+
+}
